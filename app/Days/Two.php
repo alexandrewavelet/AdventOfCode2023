58a1555725653cd79a2a79eae28451c2648e5fd8
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2023-12-02 12:52:57 +0100

    Part 2

--- app/Days/Two.php
+++ app/Days/Two.php
@@ -18,17 +18,27 @@ class Two extends Day
                     $matches,
                 );
 
-                return max($matches['red'] ?? []) <= 12
-                    && max($matches['green'] ?? []) <= 13
-                    && max($matches['blue'] ?? []) <= 14;
+                return (int)max($matches['red']) <= 12
+                    && (int)max($matches['green']) <= 13
+                    && (int)max($matches['blue']) <= 14;
             })
             ->sum(fn ($v) => (int)preg_replace("/[\D]*(\d+):.*/", '$1', $v));
     }
 
     public function secondPuzzle(): int
     {
-        return $this->dataset->sum(function ($value) {
-            return 1;
+        return $this->dataset->reduce(function ($carry, $value) {
+            $matches = [];
+            preg_match_all(
+                "/(?'red'\d+) red|(?'blue'\d+) blue|(?'green'\d+) green/",
+                $value,
+                $matches,
+            );
+
+            return (int)max($matches['red'])
+                * (int)max($matches['green'])
+                * (int)max($matches['blue'])
+                + $carry;
         });
     }
 }
--- tests/Unit/TwoTest.php
+++ tests/Unit/TwoTest.php
@@ -28,21 +28,17 @@ class TwoTest extends TestCase
     /**
      * @test
      */
-    public function it_finds_xxx(): void
+    public function it_finds_cubes_power(): void
     {
-        $this->markTestSkipped();
-
         $day = new Two(new Collection([
-            'two1nine',
-            'eightwothree',
-            'abcone2threexyz',
-            'xtwone3four',
-            '4nineeightseven2',
-            'zoneight234',
-            '7pqrstsixteen',
-            'ntxmgvkm9',
+            'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+            'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+            'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+            'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+            'Game 6: 6 red; 7 red; 8 red',
         ]));
 
-        $this->assertSame(380, $day->secondPuzzle());
+        $this->assertSame(2286, $day->secondPuzzle());
     }
 }
