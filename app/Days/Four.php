efe5011af1de8d68132a33087ae1d1dd488d2944
Alexandre Wavelet <wavelet.alexandre@gmail.com>
2023-12-05 00:38:26 +0100

    Part 2

--- app/Days/Four.php
+++ app/Days/Four.php
@@ -10,14 +10,7 @@ class Four extends Day
     public function firstPuzzle(): int
     {
         return $this->dataset->reduce(function ($carry, $scratchcard) {
-            $game = array_combine(
-                ['game', 'winners', 'scratched'],
-                preg_split("/[:|]+/", $scratchcard)
-            );
-            $winners = preg_split("/[\s]+/", trim($game['winners']));
-            $scratched = preg_split("/[\s]+/", trim($game['scratched']));
-
-            $scratchedWins = count(array_intersect($winners, $scratched));
+            $scratchedWins = $this->getWins($scratchcard);
 
             return $carry += ($scratchedWins > 0) ? pow(2, $scratchedWins - 1) : 0;
         }, 0);
@@ -25,6 +18,30 @@ class Four extends Day
 
     public function secondPuzzle(): int
     {
-        return 0;
+        $numberOfCards = $this->dataset
+            ->map(fn () => 1)
+            ->toArray();
+
+        $this->dataset->each(function ($scratchcard, $key) use (&$numberOfCards) {
+            $scratchedWins = $this->getWins($scratchcard);
+
+            for ($i = 0; $i < $scratchedWins; $i++) {
+                $numberOfCards[$key + $i + 1] += $numberOfCards[$key];
+            }
+        });
+
+        return array_sum($numberOfCards);
+    }
+
+    private function getWins(string $scratchcard): int
+    {
+        $game = array_combine(
+            ['game', 'winners', 'scratched'],
+            preg_split("/[:|]+/", $scratchcard)
+        );
+        $winners = preg_split("/[\s]+/", trim($game['winners']));
+        $scratched = preg_split("/[\s]+/", trim($game['scratched']));
+
+        return count(array_intersect($winners, $scratched));
     }
 }
